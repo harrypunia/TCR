208f188aa8342c1c29fad599c5291a9fb3212be7
Glitch (giftthecode) <none>
2018-11-18 06:35:56 +0000

    🔨😰 Checkpoint ./server.js:1341747/222

--- server.js
+++ server.js
@@ -36,42 +36,42 @@ app.post('/AddCat', (request, response) => {
     
     
     
-    var intakeDate = moment.toDate(),
-        shelterName = catObj.shelterName,
-        catname = catObj.catName,
-        primaryColour = catObj.primaryColour,
-        catWeight = catObj.catWeight,
-        fivTested = catObj.fivTested,
-        fvrcpdate = catObj.fvrcpdate,
-        catAge = catObj.catAge,
-        secondaryColour = catObj.secondaryColour,
-        gender = catObj.gender,
-        vaccineUpToDate = catObj.vaccineUpToDate,
-        spayneut = catObj.spayneut,
-        behaviour = catObj.behaviour,
-        medHist = catObj.medHist,
-        comments = catObj.comments;
+    // var intakeDate = moment.toDate(),
+    //     shelterName = catObj.shelterName,
+    //     catname = catObj.catName,
+    //     primaryColour = catObj.primaryColour,
+    //     catWeight = catObj.catWeight,
+    //     fivTested = catObj.fivTested,
+    //     fvrcpdate = catObj.fvrcpdate,
+    //     catAge = catObj.catAge,
+    //     secondaryColour = catObj.secondaryColour,
+    //     gender = catObj.gender,
+    //     vaccineUpToDate = catObj.vaccineUpToDate,
+    //     spayneut = catObj.spayneut,
+    //     behaviour = catObj.behaviour,
+    //     medHist = catObj.medHist,
+    //     comments = catObj.comments;
 
-    var columnNames = "catID, IntakeDate, Name, Photo, CurrentLocation, Neutered, VaccinationStatus, DOB, Breed, Color, Size, Sex, Weight, ShelterID, FosterPlacement, BehaviouralTraits, Story, AdoptionStatus, BittenStatus, NOTES";
-    var values = `${intakeDate}, ${shelterName}, ${catname}, ${primaryColour}, ${catWeight}, 
-                  fivTested, 
-                  fvrcpdate, 
-                  catAge, 
-                  secondaryColour, 
-                  gender, 
-                  vaccineUpToDate, 
-                  spayneut, 
-                  behaviour, 
-                  medHist, 
-                  comments
-                 ];
-    var sql = "INSERT ("+columnNames+") CAT VALUES ("+values+")";
+//     var columnNames = "catID, IntakeDate, Name, Photo, CurrentLocation, Neutered, VaccinationStatus, DOB, Breed, Color, Size, Sex, Weight, ShelterID, FosterPlacement, BehaviouralTraits, Story, AdoptionStatus, BittenStatus, NOTES";
+//     var values = `${intakeDate}, ${shelterName}, ${catname}, ${primaryColour}, ${catWeight}, 
+//                   fivTested, 
+//                   fvrcpdate, 
+//                   catAge, 
+//                   secondaryColour, 
+//                   gender, 
+//                   vaccineUpToDate, 
+//                   spayneut, 
+//                   behaviour, 
+//                   medHist, 
+//                   comments
+//                  ];
+//     var sql = "INSERT ("+columnNames+") CAT VALUES ("+values+")";
 
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-        console.log("1 record inserted");
-      });
-    con.end();
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//         console.log("1 record inserted");
+//       });
+//     con.end();
 
     response.sendFile(__dirname + '/public/Shelter/index.html');
   });
